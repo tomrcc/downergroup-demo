d237412be30a30036dd5123672c4226f6528979b
tomrcc <tom@cloudcannon.com>
2024-06-21 12:41:41 +1200

    Fix casing on news components

--- src/components/news/list.jsx
+++ src/components/news/list.jsx
@@ -1,6 +1,6 @@
 import PostSummary from './summary';
 
-export default function newsList({ posts }) {
+export default function NewsList({ posts }) {
   if (posts.length > 0) {
     return (
       <div className="grid md:grid-cols-2 xl:grid-cols-3 justify-between gap-5 mt-8 md:mt-16">
